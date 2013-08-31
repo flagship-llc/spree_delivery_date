5516bed5207722c517f2728c44f24f3b2bbb4aff
Junta <junta8918@gmail.com>
2013-09-01 00:47:16 +0900

    delete cutoff condition

--- app/models/order_decorator.rb
+++ app/models/order_decorator.rb
@@ -11,7 +11,7 @@ Spree::Order.class_eval do
   # Only run the delivery date validations if we are on that step or past.
   def delivery_date_specific_validation
     if !delivery_date.blank? && ['payment', 'confirm', 'complete'].include?(state)
-      cutoff = Time.zone.now.change(:hour => 16, :min => 00) # Gets 4pm in EST time zone (config.time_zone)
+    #  cutoff = Time.zone.now.change(:hour => 16, :min => 00) # Gets 4pm in EST time zone (config.time_zone)
 
     #  if [0, 1, 7].include?(delivery_date.wday)
     #    errors.add(:base, "cannot be a Sunday or Monday.")
@@ -20,7 +20,7 @@ Spree::Order.class_eval do
       d = Date.today
       d += 3
       
-      if cutoff.past? && !(delivery_date > d)
+      if delivery_date < d
         errors.add(:base, "配送日には本日の4日後以降の日付を指定してください")
       end
     end
