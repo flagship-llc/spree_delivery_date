744b328012baf85aa17e97aa2c3c556ad87a81c2
Keitaroh Kobayashi <keita@kkob.us>
2013-11-12 15:17:15 +0900

    adds support for validation on `earliest_shipping_date` and `latest_shipping_date`

--- app/models/order_decorator.rb
+++ app/models/order_decorator.rb
@@ -11,17 +11,22 @@ Spree::Order.class_eval do
   # Only run the delivery date validations if we are on that step or past.
   def delivery_date_specific_validation
     if !delivery_date.blank? && ['payment', 'confirm', 'complete'].include?(state)
-    #  cutoff = Time.zone.now.change(:hour => 16, :min => 00) # Gets 4pm in EST time zone (config.time_zone)
+      delivery_limited_products = products.select { |e| !e.earliest_shipping_date.nil? or !e.latest_shipping_date.nil? }
 
-    #  if [0, 1, 7].include?(delivery_date.wday)
-    #    errors.add(:base, "cannot be a Sunday or Monday.")
-    #  end
-      
       d = Date.today
       d += 3
-      
+
       if delivery_date < d
         errors.add(:base, "配送日には本日の4日後以降の日付を指定してください")
+      end
+
+      delivery_limited_products.each do |product|
+        if product.earliest_shipping_date and product.earliest_shipping_date > delivery_date
+          errors.add(:base, "#{ product.name }の配送は#{ I18n.l product.earliest_shipping_date, format: :prod_shipping_short }からとなります。")
+        end
+        if product.latest_shipping_date and delivery_date > product.latest_shipping_date
+          errors.add(:base, "#{ product.name }の配送は#{ I18n.l product.latest_shipping_date, format: :prod_shipping_short }までとなります。")
+        end
       end
     end
   end
